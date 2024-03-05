803fab78e1d8babcfd71a6b2d7bae3baef01daf5
Yonatan Israeli <yonirisraeli@gmail.com>
2024-03-05 17:49:20 -0500

    Update Windows Password Hash Exporter.bat

--- Windows Password Hash Exporter.bat
+++ Windows Password Hash Exporter.bat
@@ -2,7 +2,16 @@
 setlocal
 title Windows Password Hash Exporter
 echo Please run this batch file as an administrator.
-goto :Start
+echo READ DISCLAIMER ^-^-^> THIS IS FOR EDUCATIONAL PURPOSES ONLY! DO NOT USE THIS ON SOMEONE ELSES PC WITHOUT THEIR EXPLICIT PERMISSION! THIS IS A HACK! CONTINUE AT YOUR OWN RISK! WE HOLD NO RESPONSIBILITY FOR ANYTHING THAT HAPPEND, IS HAPPENING, AND/OR WILL HAPPEN BECAUSE OF THIS BATCH FILE!
+goto :Disclaimer
+
+:Disclaimer
+echo.
+set /p Disclaimer="Do you agree to the Disclaimer? (Yes/No) "
+if /i "%Disclaimer%"=="Yes" goto :Start
+if /i "%Disclaimer%"=="No" goto :Exit
+echo Invalid Syntax!
+goto :Disclaimer
 
 :Start
 echo.
