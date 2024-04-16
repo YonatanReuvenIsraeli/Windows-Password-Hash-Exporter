14eb4b7fd462b7ff9dd7dc1d0559dbcd958e26c1
Yonatan Israeli <yonirisraeli@gmail.com>
2024-04-15 20:01:27 -0400

    Update Windows Password Hash Exporter.bat

--- Windows Password Hash Exporter.bat
+++ Windows Password Hash Exporter.bat
@@ -2,7 +2,7 @@
 setlocal
 title Windows Password Hash Exporter
 echo Program Name: Windows Password Hash Exporter
-echo Version: 1.1.1
+echo Version: 1.1.2
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -26,6 +26,10 @@ echo.
 set HashPath=
 set /p HashPath="Where do you want to save the Windows password hashes to? "
 if not exist "%HashPath%" goto HashPathNotExist
+goto Export
+
+:Export
+if exist "%HashPath%\Windows Password Hashes" goto WindowsPasswordHashesExist
 md "%HashPath%\Windows Password Hashes"
 reg save HKLM\SAM "%HashPath%\Windows Password Hashes\SAM.save"
 reg save HKLM\SYSTEM "%HashPath%\Windows Password Hashes\SYSTEM.save"
@@ -36,6 +40,12 @@ goto Done
 :HashPathNotExist
 echo "%HashPath%" does not exist. Please try again.
 goto Start
+
+:WindowsPasswordHashesExist
+echo.
+echo Please rename or move to another location "%HashPath%\Windows Password Hashes" in order for this batch file to proceed. Press any key to continue when you have renamed or moved "%HashPath%\Windows Password Hashes".
+pause > nul 2>&1
+goto Export
 
 :Done
 endlocal
